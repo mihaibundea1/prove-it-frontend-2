e4fbadef828e3a818c8f426620a5ea58c301657a
Mihai Bundea <mihaibundea1@gmail.com>
2024-12-22 22:40:43 +0200

    removed some more junk

--- src/utils/feed.utils.ts
+++ src/utils/feed.utils.ts
@@ -1,26 +1,26 @@
-import { Post } from '../types/feed.types';
+// import { Post } from '../types/feed.types';
 
-export const feedUtils = {
-  formatPost(data: any): Post {
-    return {
-      _id: data._id,
-      userId: data.userId,
-      content: data.content,
-      images: data.images || [],
-      likes: data.likes || 0,
-      comments: data.comments || 0,
-      createdAt: data.createdAt,
-      updatedAt: data.updatedAt
-    };
-  },
+// export const feedUtils = {
+//   formatPost(data: any): Post {
+//     return {
+//       _id: data._id,
+//       userId: data.userId,
+//       content: data.content,
+//       images: data.images || [],
+//       likes: data.likes || 0,
+//       comments: data.comments || 0,
+//       createdAt: data.createdAt,
+//       updatedAt: data.updatedAt
+//     };
+//   },
 
-  formatPosts(posts: any[]): Post[] {
-    return posts.map(post => feedUtils.formatPost(post));
-  },
+//   formatPosts(posts: any[]): Post[] {
+//     return posts.map(post => feedUtils.formatPost(post));
+//   },
 
-  sortPostsByDate(posts: Post[]): Post[] {
-    return [...posts].sort((a, b) => 
-      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-    );
-  }
-};
+//   sortPostsByDate(posts: Post[]): Post[] {
+//     return [...posts].sort((a, b) => 
+//       new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+//     );
+//   }
+// };
